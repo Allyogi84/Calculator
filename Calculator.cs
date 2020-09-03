1232056380437cce541ec88e314322736c552520
Anthony Allgood <62567954+Allyogi84@users.noreply.github.com>
2020-09-03 09:32:10 -0400

    new code

---
+++ Calculator.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        public static int num1 { get; private set; }
+
+        static void Main(string[] args)
+        {
+        Console.Write("Enter a number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter another number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(num1 + num2);
+
+        Console.ReadLine();
+
+      }
+    }
+}
